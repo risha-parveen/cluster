0b175455b93b28d9a4b38e9c436531aa5b49203e
rishaaa <rishaparveen12@gmail.com>
2023-07-10 00:36:47 +0530

    dbscan clustering

--- cluster_columns.py
+++ cluster_columns.py
@@ -5,6 +5,8 @@ from itertools import product
 import spacy
 from sklearn.cluster import KMeans
 import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
 
 def preprocessing(df):
     # simple random sampling
@@ -38,22 +40,37 @@ def preprocessing(df):
             if token.has_vector:
                 vector_sum += token.vector
                 count += 1
-
         if count > 0:
             average_vector = vector_sum / count
             print(average_vector.shape)
             embeddings.append(average_vector)
+        else:
+            embeddings.append(np.zeros(shape=(300,)))
     print(len(embeddings))
     return np.array(embeddings)
+
+def kmeans_clustering(features):
+    kmeans = KMeans(init = "random", n_clusters= 5, n_init=10, max_iter= 1000, random_state=23)
+    kmeans.fit(features)
+    print(kmeans.labels_)
+
+def agglomerative_clustering(features):
+    hierarchical_cluster = AgglomerativeClustering(n_clusters=5, metric='euclidean', linkage='ward')
+    labels = hierarchical_cluster.fit_predict(features)
+    print(labels)
+
+def dbscan_clustering(features):
+    clustering = DBSCAN(eps=25, min_samples=2).fit(features)
+    print(clustering.labels_)
 
 nlp = spacy.load('en_core_web_md')
 # connecting to database
 rowcount = 100
 conn = create_engine('postgresql://postgres:password@localhost:5434/database1')
 #query for first dataset
-query1 = "SELECT * FROM public.order_reviews LIMIT %s" % rowcount
+query1 = "SELECT * FROM public.order_items LIMIT %s" % rowcount
 #query for second dataset
-query2 = "SELECT * FROM public.orders LIMIT %s" % rowcount
+query2 = "SELECT * FROM public.order_payments LIMIT %s" % rowcount
 
 df1 = pd.read_sql_query(query1.replace('%', '%%'), con = conn)
 df2 = pd.read_sql_query(query2.replace('%', '%%'), con = conn)
@@ -65,12 +82,12 @@ data = np.concatenate((data1, data2), 0)
 print(data1.shape, data2.shape, data.shape)
 
 #lets do clustering
-#kmeans clustering
-def kmeans_clustering(features):
-    kmeans = KMeans(init = "random", n_clusters= 5, n_init=10, max_iter= 100, random_state=53)
-    kmeans.fit(features)
-    print(kmeans.labels_)
-
+#kmeans clustering (partition-based)
 kmeans_clustering(data)
+#agglomerative clustering (heirarchial-clustering)
+agglomerative_clustering(data)
+#dbscan (density based)
+dbscan_clustering(data)
+
 print(df1.columns.tolist())
 print(df2.columns.tolist())
