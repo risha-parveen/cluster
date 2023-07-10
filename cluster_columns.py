78257817c7878827c59ca5bd1fe012f7a71c82ca
rishaaa <rishaparveen12@gmail.com>
2023-07-10 13:37:11 +0530

    final table after clustering

--- cluster_columns.py
+++ cluster_columns.py
@@ -8,14 +8,15 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import DBSCAN
 
+sample_values = []
 def preprocessing(df):
     # simple random sampling
     mask = df.isnull().any(axis=1)
     filtered_df = df[~mask]
     replace_it = True if (len(filtered_df) < 100) else False
-    simple_random_sample = filtered_df.sample(n=3, random_state=190, replace= replace_it)
+    simple_random_sample = filtered_df.sample(n=100, random_state=190, replace= replace_it)
     df = simple_random_sample
-
+    sample_values = df.iloc[0].values.tolist()
     string_values = []
 
     #combining values and appending together into a single array
@@ -23,8 +24,6 @@ def preprocessing(df):
         column_values = df[column].astype(str)
         joined_values = '$@'.join(column_values)
         string_values.append(joined_values)
-
-    print(string_values)
 
     embeddings = []
 
@@ -42,26 +41,32 @@ def preprocessing(df):
                 count += 1
         if count > 0:
             average_vector = vector_sum / count
-            print(average_vector.shape)
             embeddings.append(average_vector)
         else:
             embeddings.append(np.zeros(shape=(300,)))
-    print(len(embeddings))
-    return np.array(embeddings)
+    return np.array(embeddings), sample_values
+
+def add_to_table(labels, clusterLabel):
+    dataframe_data[clusterLabel] = labels
+    dataframe = pd.DataFrame(dataframe_data)
+    return dataframe
 
 def kmeans_clustering(features):
     kmeans = KMeans(init = "random", n_clusters= 5, n_init=10, max_iter= 1000, random_state=23)
     kmeans.fit(features)
-    print(kmeans.labels_)
+    table = add_to_table(kmeans.labels_, 'kmeans')
+    return table
 
 def agglomerative_clustering(features):
     hierarchical_cluster = AgglomerativeClustering(n_clusters=5, metric='euclidean', linkage='ward')
     labels = hierarchical_cluster.fit_predict(features)
-    print(labels)
+    table = add_to_table(labels, 'agglo')
+    return table
 
 def dbscan_clustering(features):
     clustering = DBSCAN(eps=25, min_samples=2).fit(features)
-    print(clustering.labels_)
+    table = add_to_table(clustering.labels_, 'dbscan')
+    return table
 
 nlp = spacy.load('en_core_web_md')
 # connecting to database
@@ -75,19 +80,26 @@ query2 = "SELECT * FROM public.order_payments LIMIT %s" % rowcount
 df1 = pd.read_sql_query(query1.replace('%', '%%'), con = conn)
 df2 = pd.read_sql_query(query2.replace('%', '%%'), con = conn)
 
-data1 = preprocessing(df1)
-data2 = preprocessing(df2)
+data1, sample1 = preprocessing(df1)
+data2, sample2 = preprocessing(df2)
 
 data = np.concatenate((data1, data2), 0)
-print(data1.shape, data2.shape, data.shape)
+
+dataframe_data = {
+    'tables': ['table 1'] + [' '] * (len(df1.columns.tolist()) - 1) +
+      ['table 2'] + [' '] * (len(df2.columns.tolist()) - 1),
+    'columns': df1.columns.tolist() + df2.columns.tolist(),
+    'sample': sample1 + sample2
+}
 
 #lets do clustering
 #kmeans clustering (partition-based)
-kmeans_clustering(data)
-#agglomerative clustering (heirarchial-clustering)
-agglomerative_clustering(data)
-#dbscan (density based)
-dbscan_clustering(data)
+kmeans_result = kmeans_clustering(data)
 
-print(df1.columns.tolist())
-print(df2.columns.tolist())
+#agglomerative clustering (heirarchial-clustering)
+agglo_result = agglomerative_clustering(data)
+
+#dbscan (density based)
+dbscan_result = dbscan_clustering(data)
+print(dbscan_result)
+
